b5c4882f956c66e8b7c45b5182212ec0be3de9c1
Kostiantyn Babich (Connie) <babich.kostia@gmail.com>
2024-07-19 18:10:53 -0700

    clean PHYS path

--- main.py
+++ main.py
@@ -5,26 +5,43 @@ import matplotlib.pyplot as plt
 
 def main():
     prerequisites: pd.DataFrame = pd.read_csv("prerequisites.csv")
-    G = nx.DiGraph()
-    nodes: dict = {}
+    G: nx.DiGraph = nx.DiGraph()
+    groups: dict[str, set[str]] = {}
+
+    color_list = ["green", "red", "blue", "purple", "orange", "yellow", "brown"]
     for line in prerequisites.values:
-        if pd.notna(line[1]) and pd.notna(line[0]):
+        if not line[2] in groups:
+            groups[line[2]] = {line[0]}
+        else:
+            groups[line[2]].add(line[0])
+
+    color_map: dict[str, str] = {}
+    for group, course_set in groups.items():
+        # retreats the last color from the color_list as the current color
+        color = color_list.pop()
+        for course in course_set:
+            if not isinstance(course, str):  # if NaN or None etc.
+                continue
+            color_map[course] = color
+
+    for line in prerequisites.values:
+        if isinstance(line[0], str) and isinstance(line[1], str):
             G.add_edge(line[1], line[0])
-        if pd.notna(line[1]):
-            G.add_node(line[1])
-        if pd.notna(line[0]):
+        elif isinstance(line[0], str):
             G.add_node(line[0])
+
     for layer, nodes in enumerate(nx.topological_generations(G)):
-        # `multipartite_layout` expects the layer as a node attribute, so add the
-        # numeric layer value as a node attribute
         for node in nodes:
             G.nodes[node]["layer"] = layer
 
-    # Compute the multipartite_layout using the "layer" node attribute
     pos = nx.multipartite_layout(G, subset_key="layer")
 
+    # assigns each node a color
+    node_colors: list[str] = [color_map[node] for node in G.nodes]
+
     fig, ax = plt.subplots()
-    nx.draw_networkx(G, pos=pos, ax=ax)
+
+    nx.draw_networkx(G, pos=pos, ax=ax, node_color=node_colors)
     ax.set_title("DAG layout in topological order")
     fig.tight_layout()
     plt.show()
